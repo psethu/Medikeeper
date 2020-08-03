97704dbe5f3e91be556d4b2cd0ca2fc1a7c5d02e
unknown <psethu27@gmail.com>
2020-08-03 05:22:09 -0400

    add Cors in API. add display in Client.

--- MedikeeperAPI/MedikeeperAPI/Startup.cs
+++ MedikeeperAPI/MedikeeperAPI/Startup.cs
@@ -25,6 +25,15 @@ namespace MedikeeperAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.SetIsOriginAllowed(_ => true)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
             services.AddMemoryCache();
             services.AddControllers();
         }
@@ -32,6 +41,8 @@ namespace MedikeeperAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("CorsPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
